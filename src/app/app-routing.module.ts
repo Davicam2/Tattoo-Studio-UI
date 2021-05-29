a102eedd78679a7cb2fc6fae2be55d09362a6048
Davicam2 <cameronwdavis88@gmail.com>
2021-05-29 14:08:22 -0400

    feat: admin route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AdminGuard } from 'src/app/auth/admin.guard';
 
 const routes: Routes = [
   {
     path: 'admin',
-    loadChildren: () => import('./views/admin-entry/admin-entry.module').then(m => m.AdminEntryModule)
+    loadChildren: () => import('./views/admin-entry/admin-entry.module').then(m => m.AdminEntryModule),
+    canActivate:[AdminGuard]
   },
   {
     path: 'public',
     loadChildren: () => import('./views/public-entry/public-entry.module').then(m => m.PublicEntryModule)
   },
+  
   {
     path:'**',
     pathMatch:'full',
--- src/app/auth/admin.guard.ts
+++ src/app/auth/admin.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { UserService } from 'src/app/services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
+
+  constructor( private user: UserService, private router: Router){ }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if(this.user.activeUser$.getValue().USER_PROFILE.isSignedIn || this.user.activeUser$.getValue().USER_PROFILE.roleID === 1){
+      return true;
+    } else {
+      return this.router.parseUrl('/public')
+    }
   }
   
 }
